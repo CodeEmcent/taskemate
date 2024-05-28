643298d4529c6f0f66b4ec7608bfe0998c463e13
Emcent <mcinnobezzy@gmail.com>
2024-05-28 23:38:24 +0100

    I am not Giving Up

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -11,6 +11,3 @@ urlpatterns = [
     path('contact-us', todolist_views.contact, name='contact'),
     path('about-us', todolist_views.about, name='about'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
