686a49317d059acfb5bd189ee8c6b76fee507375
Emcent <mcinnobezzy@gmail.com>
2024-05-28 23:29:21 +0100

    Still Trying

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
