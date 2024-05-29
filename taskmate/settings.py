cfde8aed7403eae2c4857563e1295d87eb1833e6
Emcent <mcinnobezzy@gmail.com>
2024-05-29 21:35:08 +0100

    Tials for Deployment

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -128,9 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
-STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
